9ff2541eed9150865f35d3540b3989f86b3ac906
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2024-04-18 20:26:47 -0400

    Signup form functions and route works

--- frontend/musiccatalog/src/App.jsx
+++ frontend/musiccatalog/src/App.jsx
@@ -4,11 +4,15 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 
 function App() {
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
   return (
     <>
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup setUser={setUser} />} />
       </Routes>
     </>
   );
