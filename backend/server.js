2b6791e8ea7418cfd61e695f0be9843ead5045d1
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2024-04-18 20:25:13 -0400

    Signup form and routing work - verified with test to the database

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,12 @@ const liveReloadServer = livereload.createServer();
 
 //Import routes from the controllers
 const artistController = require("./controllers/artistController");
-const musicdbRoutes = require('./controllers/musicController');
-const albumRoutes = require('./controllers/albumController');
-const reviewRoutes = require('./controllers/reviewController');
-const searchRoutes = require('./controllers/searchController');
-const genreRoutes = require('./controllers/genreController');
-const userRouter = require('./controllers/userController');
+const musicdbRoutes = require("./controllers/musicController");
+const albumRoutes = require("./controllers/albumController");
+const reviewRoutes = require("./controllers/reviewController");
+const searchRoutes = require("./controllers/searchController");
+const genreRoutes = require("./controllers/genreController");
+const userRouter = require("./controllers/userController");
 
 liveReloadServer.watch(path.join(__dirname, "../public"));
 
@@ -38,22 +38,24 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({ // Use express-session middleware
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    // Use express-session middleware
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.use(morgan('dev')); 
-
+app.use(morgan("dev"));
 
 app.use("/artists", artistController);
-app.use('/api', musicdbRoutes);
-app.use('/albums', albumRoutes)
-app.use('/reviews', reviewRoutes);
-app.use('/search', searchRoutes);
-app.use('/genre', genreRoutes);
-app.use('/users', userRouter);
+app.use("/api", musicdbRoutes);
+app.use("/albums", albumRoutes);
+app.use("/reviews", reviewRoutes);
+app.use("/search", searchRoutes);
+app.use("/genre", genreRoutes);
+app.use("/users", userRouter);
 
 // Basic route for homepage
 app.get("/", (req, res) => {
