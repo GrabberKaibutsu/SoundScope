2a7006e37c45952fc7df77fec8485b018ddfa64c
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2024-04-20 13:14:53 -0400

    Login and signup now navigates to the home page after performing these functions

--- frontend/musiccatalog/src/App.jsx
+++ frontend/musiccatalog/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/signup" element={<Signup setUser={setUser} />} />
-        {/* <Route path="/login" element={<Login setUser={setUser} />} /> */}
+        <Route path="/login" element={<Login setUser={setUser} />} />
       </Routes>
     </>
   );
