56ccac18dfdf8bbc3ee890c092dee35b7a6bb6c3
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2024-04-20 13:56:01 -0400

    Artist component created and is pulling data from spotify via our API to render data

--- frontend/musiccatalog/src/pages/Homepage.jsx
+++ frontend/musiccatalog/src/pages/Homepage.jsx
@@ -1,70 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Artists from "../components/Artists";
 
 const Homepage = () => {
+  const [homeData, setHomeData] = useState([]);
 
-    const [homeData, setHomeData] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:3001/home")
-        .then((res) => {
-            if (res.ok) {
-            return res.json();
-            }
-        })
-        .then((jsonRes) => setHomeData(jsonRes));
-    }, []);
-
-    let topArtists = homeData[0]
-    let newAlbums = homeData[1]
-    let topTracks = homeData[2]
-    console.log(topArtists)
-    console.log(newAlbums)
-    console.log(topTracks)
+  useEffect(() => {
+    fetch("http://localhost:3001/home")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((jsonRes) => setHomeData(jsonRes));
+  }, []);
+  let newAlbums = homeData[1];
+  let topTracks = homeData[2];
+  console.log(newAlbums);
+  console.log(topTracks);
 
   return (
     <div>
+      <div>
         <h1>Top Artist</h1>
+        <Artists />
+      </div>
 
-        <ul>
-            {topArtists && topArtists.map((item, index) => {
-                return (
-                    <li>
-                        {item.name}
-                        <br></br>
-                        <img src={item.images[2].url} />
-                    </li>
-                );
-            })}
-        </ul>
+      <h1>New Albums</h1>
 
-        <h1>New Albums</h1>
+      <ul>
+        {newAlbums &&
+          newAlbums.map((item, index) => {
+            return (
+              <li>
+                {item.name}
+                <br></br>
+                <img src={item.images[1].url} />
+              </li>
+            );
+          })}
+      </ul>
 
-        <ul>
-            {newAlbums && newAlbums.map((item, index) => {
-                return (
-                    <li>
-                        {item.name}
-                        <br></br>
-                        <img src={item.images[1].url} />
-                    </li>
-                );
-            })}
-        </ul>
+      <h1>Top Songs</h1>
 
-        <h1>Top Songs</h1>
-
-        <ul>
-            {topTracks && topTracks.map((item, index) => {
-                return (
-                    <li>
-                        {item.track.name}
-                    </li>
-                );
-            })}
-        </ul>
+      <ul>
+        {topTracks &&
+          topTracks.map((item, index) => {
+            return <li>{item.track.name}</li>;
+          })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
