32d065672c696b491f4ddbc091b227472c6ec0ba
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2024-04-24 19:17:41 -0400

    JWT AUTH and favoriting

---
+++ backend/controllers/validateJWT.js
@@ -0,0 +1,19 @@
+// Middleware for validating JWT tokens
+const validateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        console.error("JWT Error:", err);
+        return res.status(403).json({ message: "Invalid token" });
+      }
+      req.user = decoded;
+      next();
+    });
+  } else {
+    res.status(401).json({ message: "No token provided" });
+  }
+};
+
+module.exports = validateJWT;
