b6add11f85609e2af411e73a18cb4c03eb5cc5e4
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2024-04-25 19:56:28 -0400

    Can navigate to a single artist from the search results page

--- frontend/musiccatalog/src/App.jsx
+++ frontend/musiccatalog/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import NavBar from "./components/NavBar";
-import'./NavBar.css';
-import Search from "./pages/Search"
+import "./NavBar.css";
+import Search from "./pages/Search";
 
 import Home from "./pages/Homepage";
 import Album from "./pages/ShowAlbum";
 import Albums from "./pages/Albums";
 import Artist from "./pages/Artist";
-
 import SingleArtist from "./components/SingleArtist";
 
 function App() {
@@ -23,17 +22,16 @@ function App() {
     <>
       <NavBar />
       <br></br>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup setUser={setUser} />} />
-          <Route path="/login" element={<Login setUser={setUser} />} />
-          <Route path="/album/:id" element={<Album />} />
-          <Route path="/albums" element={<Albums />} />
-          <Route path="/artists" element={<Artist />} />
-          <Route path="/artist/:id" element={<SingleArtist />} />
-          <Route path="/search/:searchTerm" element={<Search />}/>
-        </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup setUser={setUser} />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/album/:id" element={<Album />} />
+        <Route path="/albums" element={<Albums />} />
+        <Route path="/artists" element={<Artist />} />
+        <Route path="/artist/:id" element={<SingleArtist />} />
+        <Route path="/search/:searchTerm" element={<Search />} />
+      </Routes>
     </>
   );
 }
--- frontend/musiccatalog/src/components/SearchArtists.jsx
+++ frontend/musiccatalog/src/components/SearchArtists.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-const SearchArtists = ({artists}) => {
+const SearchArtists = ({ artists }) => {
   return (
     <div className="text-slate-50">
-        <ul className="flex gap-3">
-            {/* Goes over each album in the list to display the top 20 albums */}
-            {artists && artists.map((item, index) => {
-                return (
-                    <li className="flex">
-                        {/* link to the show album page that will show more details about the album */}
-                        <Link to={`/artists/${item.id}`} className="hover:text-indigo-600">
-                            <img src={item.images[0]?.url} />
-                            {item.name}
-                        </Link>
-                    </li>
-                );
-            })}
-        </ul>
+      <ul className="flex gap-3">
+        {artists &&
+          artists.map((item, index) => {
+            return (
+              <li className="flex">
+                {/* link to the show artist page that will show more details about the album */}
+                <Link
+                  to={`/artist/${item.id}`}
+                  className="hover:text-indigo-600"
+                >
+                  <img src={item.images[0]?.url} />
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SearchArtists
+export default SearchArtists;
