36258e4712dc3f7eaec32eac6d88078c249802f9
Carson Holder <holderc2004@gmail.com>
2024-04-28 09:01:15 -0400

    album css3

--- frontend/musiccatalog/src/pages/Albums.jsx
+++ frontend/musiccatalog/src/pages/Albums.jsx
@@ -18,17 +18,16 @@ const ViewAlbums = () => {
     }, []);
 
 
-  return (
-    <div>
-        <h1 className="text-slate-50 text-4xl">New Albums</h1>
-        <br></br>
-
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 justify-items-center">
-            {/* Albums component that will show the list of the top 20 new-released albums */}
-            <Albums albums={albums} />
-        </div>
-    </div>
-  )
-}
-
-export default ViewAlbums
+    return (
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+          <h1 className="text-slate-50 text-4xl text-center mb-6">New Albums</h1>
+  
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 justify-items-center">
+              {/* Albums component that will show the list of the top 20 new-released albums */}
+              <Albums albums={albums} />
+          </div>
+      </div>
+    )
+  }
+  
+  export default ViewAlbums
