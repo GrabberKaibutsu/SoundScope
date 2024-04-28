e73c253b1d0effa72861f1ace2995b92ffb841d6
Carson Holder <holderc2004@gmail.com>
2024-04-28 09:12:52 -0400

    playlist css

--- frontend/musiccatalog/src/pages/FeaturedPlaylists.jsx
+++ frontend/musiccatalog/src/pages/FeaturedPlaylists.jsx
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const host = import.meta.env.VITE_BACKENDURL
-const FeaturedPlaylists = ({ }) => {
+const host = import.meta.env.VITE_BACKENDURL;
+
+const FeaturedPlaylists = () => {
     const [playlists, setPlaylists] = useState([]);
 
     useEffect(() => {
-        fetch(`${host}/api/featured-playlists`)//added api
-            .then(response => response.json())
+        fetch(`${host}/api/featured-playlists`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to fetch playlists');
+                }
+            })
             .then(data => setPlaylists(data))
             .catch(error => console.error('Error fetching playlists:', error));
     }, []);
 
-return (
-    <div className="px-10 py-5">
-      <h1 className="text-4xl text-slate-50 mb-5">Featured Playlists</h1>
-      <div className="grid grid-cols-3 gap-5">
-        {playlists.map(playlist => (
-          <Link key={playlist.id} to={`/playlists/${playlist.id}`} className="bg-gray-800 hover:bg-gray-700 rounded-lg overflow-hidden shadow-lg transition duration-200 ease-in-out block">
-            <img src={playlist.images[0]?.url} alt={playlist.name} className="w-full h-64 object-cover" />
-            <div className="p-4">
-              <h3 className="text-xl text-slate-50">{playlist.name}</h3>
-              <p className="text-gray-400">{playlist.description}</p>
+    return (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+            <h1 className="text-4xl text-slate-50 mb-5">Featured Playlists</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                {playlists.map(playlist => (
+                    <Link key={playlist.id} to={`/playlists/${playlist.id}`} className="bg-gray-800 hover:bg-gray-700 rounded-lg overflow-hidden shadow-lg transition duration-200 ease-in-out block">
+                        <img src={playlist.images[0]?.url} alt={playlist.name} className="w-full h-64 object-cover" />
+                        <div className="p-4">
+                            <h3 className="text-xl text-slate-50">{playlist.name}</h3>
+                            <p className="text-gray-400">{playlist.description}</p>
+                        </div>
+                    </Link>
+                ))}
             </div>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
-
 export default FeaturedPlaylists;
-
